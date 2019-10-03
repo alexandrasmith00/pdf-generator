f574c42dc19cd80a317da264556957820d1f2aac
Lexi Smith <alexandrasmith00@gmail.com>
2019-10-03 18:27:52 -0400

    Separates build logic from default generate

---
+++ createPdf.js
@@ -0,0 +1,18 @@
+const { generatePdf } = require('./index');
+
+// gets the template name from CL args
+const template = process.argv[2];
+
+if (template) {
+  (async () => {
+    // let user know we've started process
+    console.log('Building the PDF...');
+
+    // generates a pdf and stores in the templates directory
+    const options = { path: `./templates/${template}/output.pdf`, format: 'A4' };
+    const path = await generatePdf(template, null, options);
+
+    // on success, let user know where to find output
+    console.log(`\n\nPDF built at: ${path}\n\n`);
+  })();
+} else { console.log('Please specify a template.'); }
--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 const { generatePdf } = require('tea-school');
 
-const getOptions = (template, data = null) => ({
-  htmlTemplatePath: `./templates/${template}/template.pug`,
-  styleOptions: { file: `./templates/${template}/styles.scss` },
-  htmlTemplateOptions: data || require(`./templates/${template}/data.js`),
-  pdfOptions: { path: `./templates/${template}/output.pdf`, format: 'A4' },
-});
+const createOptions = (name, data = null, pdfOptions) => ({
+  htmlTemplatePath: `./templates/${name}/template.pug`,
+  styleOptions: { file: `./templates/${name}/styles.scss` },
+  htmlTemplateOptions: data || require(`./templates/${name}/data.js`),
+  pdfOptions: { format: 'A4', ...pdfOptions },
+})
 
-const template = process.argv[2];
-if (template) {
-  (async () => {
-    console.log('Building pdf...');
-    const options = getOptions(template);
-    await generatePdf(options);
-    console.log(`\n\nPDF built at: templates/${template}/output.pdf\n\n`);
-  })();
-} else {
-  console.log('Please specify which template you want built.')
-}
+module.exports = {
+  generatePdf: async (name, data, pdfOptions = {}) => {
+    const options = createOptions(name, data, pdfOptions);
+    const result = await generatePdf(options);
+    return pdfOptions.path || Buffer.from(result).toString('base64');
+  },
+};
