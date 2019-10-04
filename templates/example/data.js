956a49e552c32d02ad7e19bbf1b83a540e90140f
jmleczkoer <jmleczkoer@gmail.com>
2019-10-03 20:26:17 -0400

    add example template

---
+++ templates/example/data.js
@@ -0,0 +1,36 @@
+module.exports = {
+  // data goes here...
+  message: 'Hello!',
+  items: [
+    {
+      name: "Sausage",
+      price: "7.50",
+      code: "SAU",
+      notes: "3 per pack",
+    },
+    {
+      name: "Sausage",
+      price: "7.50",
+      code: "SAU",
+      notes: "3 per pack",
+    },
+    {
+      name: "Sausage",
+      price: "7.50",
+      code: "SAU",
+      notes: "3 per pack",
+    },
+    {
+      name: "Sausage",
+      price: "7.50",
+      code: "SAU",
+      notes: "3 per pack",
+    },
+    {
+      name: "Sausage",
+      price: "7.50",
+      code: "SAU",
+      notes: "3 per pack",
+    },
+  ],
+};
